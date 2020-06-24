f2065b29314cf09d6e4e74835938e62abe9eaee8
Abbosbek Muydinboev <muydinboevabbosbek@gmail.com>
2020-06-24 04:15:45 +0000

    Automatically backed up by Learn

--- lib/find_in_array.rb
+++ lib/find_in_array.rb
@@ -1,10 +1,10 @@
 def find_element_index(array, value_to_find) 
   i = 0 
   while array[i] do
-    if array[i] == value_to_find
-      array.index(array[i])
+    if array[i] != value_to_find
+      nil
     end
     i += 1
   end
-  nil
+  return array.index(array[i])
 end
